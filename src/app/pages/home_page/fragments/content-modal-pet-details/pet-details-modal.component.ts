484c7699f99fec965cff2ad7a17ac9a81f9bd84d
Danison dos Santos <danison.santos00@gmail.com>
2023-12-26 20:00:32 -0300

    add

---
+++ src/app/pages/home_page/fragments/content-modal-pet-details/pet-details-modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Animal } from 'src/app/models/animal';
+
+@Component({
+  selector: 'app-pet-details-modal',
+  templateUrl: './pet-details-modal.component.html',
+  styleUrls: ['./pet-details-modal.component.scss']
+})
+export class PetDetailsModalComponent {
+
+  @Input() pet!: Animal;
+  @Output() eventClose = new EventEmitter<void>();
+  @Output() eventInterest = new EventEmitter<void>();
+
+
+  clickClose() {
+    this.eventClose.emit()
+  }
+  clickInterest() {
+    this.eventInterest.emit()
+  }
+}
