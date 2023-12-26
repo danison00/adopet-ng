f8f332d55b1f84914d8a1a41e2d3cfdaf47931e4
Danison dos Santos <danison.santos00@gmail.com>
2023-12-26 19:59:59 -0300

    fix

--- src/app/pages/home_page/fragments/card-perfil-animal/card-perfil-animal.component.ts
+++ src/app/pages/home_page/fragments/card-perfil-animal/card-perfil-animal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Animal } from 'src/app/models/animal';
 
 @Component({
@@ -8,9 +8,19 @@ import { Animal } from 'src/app/models/animal';
 })
 export class CardPerfilAnimalComponent {
 
-  animal: Animal = {
+  @Input() animal: Animal = {
+    imgPath: "/assets/img/cao-adoravel-olhando-para-cima-em-um-estudio.jpg",
     nome: "Francisco",
+    castrado: true,
+    cuidadosEspeciais: false,
+    descricao: "um dog carinhoso, simpático e com muit energia.",
+    sexo: "macho",
     caracteristica: "Carinhoso",
     idade: 5
+  }
+  @Output() eventClickcard = new EventEmitter<Animal>();
+  clickCard(event: MouseEvent){
+    this.eventClickcard.emit(this.animal);
+    
   }
 }
