b1a5ed99431db5d5075fd9def2585f1dd578b523
Danison dos Santos <danison.santos00@gmail.com>
2023-12-26 19:59:13 -0300

    update

--- src/app/pages/home_page/home/home.component.ts
+++ src/app/pages/home_page/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, HostListener } from '@angular/core';
+import { bindCallback } from 'rxjs';
+import { Animal } from 'src/app/models/animal';
+import { UtilService } from 'src/app/service/util.service';
+import { __await } from 'tslib';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +12,36 @@ import { Component, HostListener } from '@angular/core';
 export class HomeComponent {
 
   boxSearchVisible = false;
+  openModalPetDetails = false;
+  openModalSuccessfulAdoption = false;
+  pets!: Animal[];
 
-  onArrowClick() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    setTimeout(() => {
+  pet: Animal = {
+    caracteristica: "Agitado",
+    castrado: true,
+    cuidadosEspeciais: false,
+    descricao: "Doguinho muito dócil mas muito agitado",
+    idade: 5,
+    imgPath: "/assets/img/dog-francisco.svg",
+    nome: "Francisco",
+    sexo: "macho"
 
-      this.boxSearchVisible = !this.boxSearchVisible;
-    }, 650);
+  }
+  constructor(private util: UtilService) {
+    this.pets = util.dogs;
+  }
+
+  onClickCard(pet: Animal) {
+    this.pet = pet;
+    this.openModalPetDetails = true;
+  }
+
+  async onArrowClick() {
+    await this.scrollTop();
+    this.setBoxSearchVisible();
   }
   @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event): void {
-    // Obtém a posição do topo do componente em relação à parte superior da janela
+  private onScroll(event: Event): void {
     const componentTop = document.getElementById('closeBtn');
     if (componentTop) {
       if (componentTop.getBoundingClientRect().top < 0)
@@ -26,5 +49,33 @@ export class HomeComponent {
 
     }
   }
+  getPositionScroll(): any {
+    return document.getElementById('header')?.getBoundingClientRect().top;
+  }
+
+
+  setBoxSearchVisible() {
+
+    this.boxSearchVisible = !this.boxSearchVisible;
+
+  }
+  async delay() {
+    return new Promise((resolve) => setTimeout(resolve, 100));
+
+  }
+
+  async scrollTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    while (true) {
+      if (this.getPositionScroll() === 0) break;
+      await this.delay();
+    }
+  }
+
+  clickInterestIntPet() {
+    this.openModalPetDetails = false;
+    this.openModalSuccessfulAdoption = true;
+  }
+
 
 }
