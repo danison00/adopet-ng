b923ec4db567d25ed2ae6c902c98e5b5f51a8619
Danison dos Santos <danison.santos00@gmail.com>
2023-12-27 21:39:20 -0300

    add

--- src/app/components/header/fragments/menu-meus-pets/menu-meus-pets.component.ts
+++ src/app/components/header/fragments/menu-meus-pets/menu-meus-pets.component.ts
@@ -11,30 +11,5 @@ export class MenuHeaderComponent {
   @Output() eventCloseMenu = new EventEmitter<void>();
 
   @Input() openMenu = false;
-  clickEvent(event: MouseEvent) {
-    if (event.target === event.currentTarget) {
 
-      this.eventCloseMenu.emit();
-    }
-  }
-  @HostListener('window:click', ['$event'])
-  private onScroll(event: Event): void {
-
-    if(!this.clickInMenu(event)){
-      this.eventCloseMenu.emit();
-
-    }
-
-  }
-  public clickInMenu(event: Event): boolean {
-    let links = document.getElementsByClassName("link-menu-meus-pets");
-    for (let i = 0; i < links.length; i++) {
-
-      if (links.item(i) === event.target) {
-        return true;
-      } 
-
-    }
-    return false;
-  }
 }
