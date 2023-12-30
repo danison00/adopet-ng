6c826aa98527c100c78e8ddf043ef9e71eca0e18
Danison dos Santos <danison.santos00@gmail.com>
2023-12-30 19:28:42 -0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AdopetComponent } from './pages/adopet/adopet.component';
 import { HomeComponent } from './pages/home_page/home/home.component';
 import { MyPetsToDonatePageComponent } from './pages/my-pets-to-donate-page/my-pets-to-donate-page.component';
 import { HomeOutletComponent } from './pages/home-outlet/home-outlet.component';
+import { DonatePetPageComponent } from './pages/donate-pet-page/donate-pet-page.component';
 
 const routes: Routes = [
   {path: "", component: AdopetComponent},
   {path: "login", component: LoginComponent},
   {path: "home", component: HomeOutletComponent, children:[
     {path: "", component: HomeComponent},
-    {path: "meus-pets/para-adocao", component: MyPetsToDonatePageComponent}
+    {path: "meus-pets/para-adocao", component: MyPetsToDonatePageComponent},
+    {path: "doar-pet", component: DonatePetPageComponent}
   ]},
   
 ];
