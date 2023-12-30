4a465042b26461ab2705d9318cecda8d2381a7b8
Danison dos Santos <danison.santos00@gmail.com>
2023-12-29 23:54:06 -0300

    up

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -7,15 +7,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ButtonComponent {
 
-  @Input() text="Button";
-  @Input() type : "primary" | "secondary" | 'terciary' | 'ligth-blue' | 'ligth-orange' | 'ligth-red' = "primary";
+  @Input() text = "Button";
+  @Input() type: "primary" | "secondary" | 'terciary' |
+    'ligth-blue' | 'ligth-orange' | 'ligth-red' | 'gray' = "primary";
   @Input() hoverScale = true;
-  @Input() width100 : false | true = false;
-  @Input() bg : 'dark-blue' | 'blue' | 'none' = 'none';
+  @Input() width100: false | true = false;
+  @Input() bg: 'dark-blue' | 'blue' | 'none' = 'none';
 
   @Output() clickEvent: EventEmitter<void> = new EventEmitter<void>();
 
-  emittClick(){
+  emittClick() {
     this.clickEvent.emit();
   }
 }
