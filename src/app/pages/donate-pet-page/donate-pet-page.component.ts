3d9c45a4b2f37e57064bf9559bcbf5466a84afc9
Danison dos Santos <danison.santos00@gmail.com>
2024-01-01 17:38:50 -0300

    fix

--- src/app/pages/donate-pet-page/donate-pet-page.component.ts
+++ src/app/pages/donate-pet-page/donate-pet-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-donate-pet-page',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
 })
 export class DonatePetPageComponent {
 
+  form: FormGroup;
+  modalRegisterSuccess = false;
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      imagem: [null, Validators.required],
+      especie: ['', Validators.required],
+      nome: ['', [Validators.required, Validators.minLength(2)]],
+      idade: [null, [Validators.required]],
+      sexo: ['', Validators.required],
+      cidade: ['', Validators.required],
+      estado: ['', Validators.required],
+      caracteristicas: ['', Validators.required],
+      castrado: [false, Validators.required],
+      cuidadosEspeciais: [false, Validators.required],
+    });
+  }
 
   petsMaisComuns = [
     'Cachorro',
@@ -20,8 +38,28 @@ export class DonatePetPageComponent {
     'Furão'
   ];
 
-  onEnviar(){
+  onEnviar() {
+
+    if (this.form.invalid) {
+      return
+    }
+    this.modalRegisterSuccess = true;
     console.log("enviando...");
+    console.log(this.form.value);
+    
+  }
+  maskAge(){
+    let age = parseInt(this.form.get('idade')?.value)
+    
+    if(age < 0 ){
+      this.form.patchValue({idade: 0})
+      return;
+    }
+    if(age > 20 ){
+      this.form.patchValue({idade: 20})
+      return;
+    }
+
     
   }
 
